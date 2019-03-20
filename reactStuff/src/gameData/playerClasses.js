8810a48bc291542bf116dccfc323b2d5a621a8e7
Nathan Hill <nateohill@gmail.com>
2019-03-20 15:50:31 -0700

    Added basic structure for adventurer classes later on

---
+++ reactStuff/src/gameData/playerClasses.js
@@ -0,0 +1,82 @@
+export default {
+  "Physical Class": [
+    {
+      name: "Blade Master",
+      desc: "Furious Warrior that deals extreme damage when fully enraged."
+    },
+  
+    {
+      name: "Crusader",
+      desc: "Armored Warrior that has extreme resilience."
+    },
+  
+    {
+      name: "Dark Ranger",
+      desc: "Assassin archer, has stealth, carries poison arrows and daggers."
+    },
+  
+    {
+      name: "Beast Master",
+      desc: "Forest Archer, can tame Beasts. Tamed Beasts have varying abilities. Deals Nature damage."
+    },
+  
+    {
+      name: "Marksman",
+      desc: "Specializes in ranged bows or guns, has a focus bar. Damage dealt is higher if high focus is maintained."
+    }
+  ],
+
+  "Elemental Class": [
+    {
+      name: "Wind mage",
+      desc: "A young mage who comes from the grasslands in the east"
+    },
+
+    {
+      name: "Frost mage",
+      desc: "A mage that comes from the icey North to avenge his mother"
+    },
+  
+    {
+      name: "Fire mage",
+      desc: "A Mage that calls upon the power of the flame"
+    },
+
+    {
+      name: "Warlock",
+      desc: "A Warlock that can ressurect dead monsters as their minions. Uses Shadow magic, armed with a Scythe or Staff."
+    }
+  ],
+  
+  "Quest Class": [
+    {
+      name: "Time Traveler",
+      desc: "A Mage that uses the power of personal time travel to manipulate or change events. Can use time travel to heal themself or remove debuffs. Uses Arcane spells."
+    },
+
+    {
+      name: "Firelord",
+      desc: "A Mage that calls upon the power of the flame"
+    },
+
+    {
+      name: "Solaris",
+      desc: "A warrior of the sun, resistant to fire attacks, can imbue plasma magic onto weapons, and starts with solar flare."
+    },
+  
+    {
+      name: "Frost Weaver",
+      desc: "A Mage that uses Ice Powers."
+    },
+
+    {
+      name: "Necromancer",
+      desc: "A Warlock that can ressurect dead monsters as their minions. Uses Shadow magic, armed with a Scythe or Staff."
+    },
+  
+    {
+      name: "Harvester",
+      desc: "A Warlock that uses shadow magic to reap souls for healing benefit."
+    }
+  ]
+};
