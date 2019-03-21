cfc9ac0fe51ef504aee4acc66a6f9401b79dd157
Nathan Hill <nateohill@gmail.com>
2019-03-21 14:04:37 -0700

    Allowed for multiple level ups to occur at once

--- reactStuff/src/components/screens/ActionScreen.jsx
+++ reactStuff/src/components/screens/ActionScreen.jsx
@@ -5,21 +5,16 @@ export default withRouter(({player, playerStatus, updatePlayerStatus, updateStat
   if (!player.isCreated) {
     history.push('/');
   }
-  if (player.exp >= player.expToNextLevel) {
+  while (player.exp >= player.expToNextLevel) {
     player.exp -= player.expToNextLevel;
     player.level++;
     player.skillPoints += 5;
-    player.stats.strength++;
-    player.stats.agility++;
-    player.stats.intelligence++;
-    player.stats.charisma++;
-    player.stats.will++;
     player.levelUp();
-    updateStatusText(`Congratulations! You leved up to level ${player.level}!
-    How would you like to distribute your stats? (since this is still in beta +1 to all stats :3)
-    `)
-    updatePlayerStatus(`You have ${player.health} health and ${player.mana} mana.`);
   }
+  updateStatusText(`Congratulations! You leved up to level ${player.level}!
+  How would you like to distribute your stats? (since this is still in beta +1 to all stats :3)
+  `)
+  updatePlayerStatus(`You have ${player.health} health and ${player.mana} mana.`);
   return (
     <div>
       <div className="player-options">
